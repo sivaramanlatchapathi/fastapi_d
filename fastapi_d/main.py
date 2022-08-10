526b725cf35c4bdb90ec368771cbd2378ff088b3
sivaramanlatchapathi <sivaraman.latchapathi@gmail.com>
2022-08-10 16:01:57 -0400

    start-app script setup

--- fastapi_d/main.py
+++ fastapi_d/main.py
@@ -1,7 +1,12 @@
+from imp import reload
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
 def index():
     return {"hello":"Fast API"}
+
+def start_app():
+    uvicorn.run("fastapi_d.main:app", host="0.0.0.0", port=8000, reload=True)
